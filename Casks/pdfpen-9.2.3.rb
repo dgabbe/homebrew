56a68101ce8c7b9941227ee3ca95e7797287092b
David G <dgabbe@acm.org>
2018-05-10 11:51:44 -0400

    Initial commit.

---
+++ Casks/pdfpen-9.2.3.rb
@@ -0,0 +1,10 @@
+cask 'pdfpen-9.2.3' do
+  version '9.2.3'
+  sha256 '9c93c5350abd73e474911cf800a44ac197700e800cd6163f4dd4a5ffe5049357'
+
+  url 'https://dl.smilesoftware.com/com.smileonmymac.PDFpen/923.0/1510025892/PDFpen-923.0.zip'
+  name 'PDFpen'
+  homepage 'https://smilesoftware.com/pdfpen'
+
+  app 'PDFpen.app'
+end
