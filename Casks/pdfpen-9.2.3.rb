3ce0999acef958355f01f353d9c4665aea460cbe
David G <dgabbe@acm.org>
2019-10-25 14:03:05 -0400

    Trying to fix homebrew cask error...

--- Casks/pdfpen-9.2.3.rb
+++ Casks/pdfpen-9.2.3.rb
@@ -1,6 +1,6 @@
 cask 'pdfpen-9.2.3' do
   version '9.2.3'
-  depends_on macos: ['10.13']
+  depends_on macos: :sierra
   sha256 '9c93c5350abd73e474911cf800a44ac197700e800cd6163f4dd4a5ffe5049357'
 
   url 'https://dl.smilesoftware.com/com.smileonmymac.PDFpen/923.0/1510025892/PDFpen-923.0.zip'
